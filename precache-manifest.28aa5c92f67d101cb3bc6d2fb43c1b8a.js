1a383c39acf84a1e643141f6b6c13dc96f4c55cd
Lucas Pessone <lucas.rmagalhaes@gmail.com>
2020-01-09 18:21:13 -0200

    Updates

---
+++ precache-manifest.28aa5c92f67d101cb3bc6d2fb43c1b8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55575407b06190d8911cb953f7f3ae42",
+    "url": "/fcc-markdown-preview/index.html"
+  },
+  {
+    "revision": "3258dfd99123f7614049",
+    "url": "/fcc-markdown-preview/static/css/main.70efd524.chunk.css"
+  },
+  {
+    "revision": "7d40cbb6dd42c0f40a2d",
+    "url": "/fcc-markdown-preview/static/js/2.ca408190.chunk.js"
+  },
+  {
+    "revision": "76d7a8da27b1246d0ec7305dafc313ce",
+    "url": "/fcc-markdown-preview/static/js/2.ca408190.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3258dfd99123f7614049",
+    "url": "/fcc-markdown-preview/static/js/main.fc2d081c.chunk.js"
+  },
+  {
+    "revision": "484b2aae6aaf952037e5",
+    "url": "/fcc-markdown-preview/static/js/runtime-main.90e1a247.js"
+  }
+]);
